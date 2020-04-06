b5c7e6f95c3101df80bd4c2ad6e534772ab75ca2
thak1411 <thak1411@gmail.com>
2020-04-06 20:46:22 +0900

    update header props

--- frontend/src/views/header/header.vue
+++ frontend/src/views/header/header.vue
@@ -1,18 +1,31 @@
 <template lang="pug">
-header.guava-header
+header.guava-header(:style="style")
     | logo
 </template>
 
 <script>
+import Util from '../components/js/util.js';
+
 export default {
     name: 'guava-header',
+    props: {
+        init_height: {
+            default: 62,
+        },
+    },
+    computed: {
+        style: function() {
+            return {
+                height: Util.mU(this.init_height),
+            };
+        },
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .guava-header {
     width: 100%;
-    height: 62px;
     background-color: #2060ff;
 }
 </style>
