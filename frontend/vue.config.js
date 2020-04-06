96d44eae0b90c5980ed5475847b5471a32152191
thak1411 <thak1411@gmail.com>
2020-04-06 20:00:09 +0900

    update pages grid

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,13 +1,11 @@
-const ansiRegex = require('ansi-regex');
 module.exports = {
     pages: {
         index: {
             entry: './src/pages/index/main.js',
             template: './public/index.html',
-            filename: 'index.out.html',
+            filename: 'index.html',
         },
     },
     lintOnSave: false,
     filenameHashing: false,
-    transpileDependencies: [ansiRegex],
 }
