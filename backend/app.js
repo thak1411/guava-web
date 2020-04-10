5582a0e8a49fec8dd819eae506ae958ac9d4ced2
thak1411 <thak1411@gmail.com>
2020-04-10 22:24:53 +0900

    add backend server setting

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,29 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const path = require('path');
+const cors = require('cors');
+const logger = require('morgan');
+const express = require('express');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'dist'));
 
+app.use(cors());
 app.use(logger('dev'));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/js', express.static(path.join(__dirname, 'dist', 'js')));
+app.use('/css', express.static(path.join(__dirname, 'dist', 'css')));
+app.use('/img', express.static(path.join(__dirname, 'dist', 'img')));
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +38,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
+//   res.render('error');
 });
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+const router = require('express').Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.sendfile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
 module.exports = router;
