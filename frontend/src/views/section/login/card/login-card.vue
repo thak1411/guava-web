b9b5a8f10e04ae212b71d4a24f92bd1561e76157
thak1411 <thak1411@gmail.com>
2020-04-14 21:34:34 +0900

    add login card

---
+++ frontend/src/views/section/login/card/login-card.vue
@@ -0,0 +1,81 @@
+<template lang="pug">
+div.login-card
+    ctxt.card-title(:init_message="$t('login.title')" init_color="#000000" :init_fontSize="18")
+    div.seperate-line
+    input.card-input(v-model="id" :placeholder="$t('login.id')")
+    input.card-input(v-model="password" :placeholder="$t('login.password')")
+    button.card-login(@click="onSubmit")
+        ctxt(:init_message="$t('login.submit')" init_color="#ffffff" :init_fontSize="16")
+    div.seperate-line.mtb10
+    ctxt(init_message="비밀번호 잊어버리면 알아서 찾으세요" init_color="#000000" :init_fontSize="16")
+</template>
+
+<script>
+import ctxt from '../../../components/ctxt.vue';
+
+export default {
+    name: 'login-card',
+    components: {
+        ctxt,
+    },
+    data: function() {
+        return {
+            id: '',
+            password: '',
+        };
+    },
+    methods: {
+        onSubmit: function() {
+            
+        },
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.login-card {
+    width: 300px;
+    height: 300px;
+    display: flex;
+    padding: 20px;
+    margin-left: auto;
+    margin-right: auto;
+    border-radius: 4px;
+    flex-direction: column;
+    box-sizing: border-box;
+    border: 1px solid #ececec;
+}
+.card-title {
+    text-align: center;
+    margin-bottom: 5px;
+}
+.card-input {
+    width: 100%;
+    height: 26px;
+    outline: none;
+    font-size: 14px;
+    margin-top: 10px;
+    text-indent: 15px;
+    border-radius: 8px;
+    display: inline-block;
+    border: 1px solid #ececec;
+}
+.card-login {
+    width: 100%;
+    height: 26px;
+    border: none;
+    outline: none;
+    margin-top: 10px;
+    border-radius: 20px;
+    background: linear-gradient(90deg, rgb(31, 202, 74) 0%, rgb(255, 121, 121) 100%);
+}
+.seperate-line {
+    width: 100%;
+    height: 1px;
+    border-bottom: 1px solid #ececec;
+}
+.mtb10 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+</style>
--- frontend/src/views/section/login/section.vue
+++ frontend/src/views/section/login/section.vue
@@ -1,16 +1,41 @@
 <template lang="pug">
 section.login-section
-    | login section
+    div.title-slot
+        ctxt(:init_message="$t('login.title')" :init_fontSize="26" init_fontColor="#000000" :init_fontWeight="800")
+    div.content-slot
+        login-card
 </template>
 
 <script>
+import ctxt from '../../components/ctxt.vue';
+import LoginCard from './card/login-card.vue';
+
 export default {
     name: 'login-section',
+    components: {
+        ctxt,
+        LoginCard,
+    },
 }
 </script>
 
 <style lang="scss" scoped>
 .login-section {
     width: 100%;
+    display: flex;
+    flex-direction: column;
+}
+.title-slot {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    margin: 20px 0 0 15px;
+}
+.content-slot {
+    width: 100%;
+    display: flex;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    flex-direction: row;
 }
 </style>
