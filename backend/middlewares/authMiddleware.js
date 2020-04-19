666eb396536393ff036c3ec3d9e0f53b64338baf
thak1411 <thak1411@gmail.com>
2020-04-20 00:03:34 +0900

    add auth middleware

---
+++ backend/middlewares/authMiddleware.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config');
+
+const auth = {
+    checkUser: function (req, res, next) {
+        const token = req.cookies[config.cookie.session];
+        if (!token) {
+			const error = new Error("not logged in");
+			error.status = 401;
+			return next(error);
+        }
+        jwt.verify(token, req.app.get("jwt-secret"), (err, decoded) => {
+			if (err) {
+				if (err.name === "TokenExpiredError") {
+					const error = new Error(err);
+					error.status = 9401;
+					res.clearCookie(config.cookie.session);
+					return next(error);
+				} else {
+					const error = new Error(err);
+					error.status = 9401;
+					return next(error);
+				}
+			}
+
+            req.token = decoded;
+			return next();
+		});
+    }
+};
+
+module.exports = auth;
